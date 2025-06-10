c26cf306d63b7778651522759cd243b3ed6b6053
kanhaiya-v <kanahaiya@lyzr.ai>
2025-06-10 20:30:56 +0530

    Updated lyfe config, routes and component.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
+      <AppRoutes />
     </div>
   );
 }
